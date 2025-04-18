19dde26e888203f5d360ea6e4a62ae7f9c30f616
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2025-04-17 18:29:18 -0600

    feat: define new constant

--- core/Config.php
+++ core/Config.php
@@ -3,6 +3,9 @@
 // if the initial route is a root route, it should be empty
 // example, if the initial route is 'home', it should be 'home' and not '/home'
 define('INITIAL_ROUTE', '');
+define('ASSET_IMG', INITIAL_ROUTE . '/assets/img/');
+define('ASSET_CSS', INITIAL_ROUTE . '/assets/css/');
+define('ASSET_JS', INITIAL_ROUTE . '/assets/js/');
 define('CREDENTIALS', json_decode(file_get_contents(__DIR__ . '/../credentials.json'), true));
 
 
@@ -10,5 +13,3 @@ define('CREDENTIALS', json_decode(file_get_contents(__DIR__ . '/../credentials.j
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-
-
